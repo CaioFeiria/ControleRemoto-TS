87061ef183fe6c29bc9d358d3d52bb00b30d0567
caio <caiofeiria@proton.me>
2025-01-16 11:10:08 -0300

    Implementando Classe e Enum

--- index.js
+++ index.js
@@ -1,18 +1,68 @@
-//#region Controle1
-var remote1 = {
-    color: "Preto",
-    brand: "Samsung",
-    volume: 20,
-    channel: 1,
-    power: false,
-    showControlStatus: function () {
-        console.log("\n      \u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n      \u2551         STATUS DO CONTROLE         \u2551\n      \u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n      \u2551 Volume         : ".concat(this.volume, "                \u2551\n      \u2551 Canal          : ").concat(this.channel, "                 \u2551\n      \u2551 Cor do Controle: ").concat(this.color, "             \u2551\n      \u2551 Marca da TV    : ").concat(this.brand, "           \u2551\n      \u2551 Estado da TV   : ").concat(this.power ? "Ligada" : "Desligada", "            \u2551\n      \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n    "));
-    },
-    togglePower: function () {
-        this.power = !this.power;
-        console.log("A TV est\u00E1 ".concat(this.power ? "Ligada" : "Desligada"));
-    },
-    increaseVolume: function () {
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Mode;
+(function (Mode) {
+    Mode["Cool"] = "COOL";
+    Mode["Heat"] = "HEAT";
+    Mode["Fan"] = "FAN";
+})(Mode || (Mode = {}));
+var Power;
+(function (Power) {
+    Power["On"] = "Ligado";
+    Power["Off"] = "Desligado";
+})(Power || (Power = {}));
+var RemoteControl = /** @class */ (function () {
+    function RemoteControl(cor, marca, volume, canal) {
+        this.color = cor;
+        this.brand = marca;
+        this.volume = volume;
+        this.channel = canal;
+        this.power = Power.Off;
+    }
+    Object.defineProperty(RemoteControl.prototype, "getPower", {
+        get: function () {
+            return this.power;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(RemoteControl.prototype, "getColor", {
+        get: function () {
+            return this.color;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(RemoteControl.prototype, "getBrand", {
+        get: function () {
+            return this.brand;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    RemoteControl.prototype.togglePower = function () {
+        if (this.power === Power.Off) {
+            this.power = Power.On;
+        }
+        else {
+            this.power = Power.Off;
+        }
+        console.log("O Dispositvo est\u00E1 ".concat(this.power));
+    };
+    RemoteControl.prototype.increaseVolume = function () {
         if (this.power) {
             if (this.volume < 100) {
                 this.volume += 10;
@@ -25,8 +75,8 @@ var remote1 = {
         else {
             console.log("A TV ".concat(this.brand, " est\u00E1 Desligada! Ligue para conseguir interagir."));
         }
-    },
-    decreaseVolume: function () {
+    };
+    RemoteControl.prototype.decreaseVolume = function () {
         if (this.power) {
             if (this.volume > 0) {
                 this.volume -= 10;
@@ -39,8 +89,8 @@ var remote1 = {
         else {
             console.log("A TV ".concat(this.brand, " est\u00E1 Desligada! Ligue para conseguir interagir."));
         }
-    },
-    changeChannel: function (newChannel) {
+    };
+    RemoteControl.prototype.changeChannel = function (newChannel) {
         if (this.power) {
             if (newChannel > 0) {
                 this.channel = newChannel;
@@ -53,8 +103,14 @@ var remote1 = {
         else {
             console.log("A TV ".concat(this.brand, " est\u00E1 Desligada! Ligue para conseguir interagir."));
         }
-    },
-};
+    };
+    RemoteControl.prototype.showControlStatus = function () {
+        console.log("\n      \u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n      \u2551       STATUS DO CONTROLE TV        \u2551\n      \u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n      \u2551 Volume         : ".concat(this.volume, "                \u2551\n      \u2551 Canal          : ").concat(this.channel, "                 \u2551\n      \u2551 Cor do Controle: ").concat(this.color, "             \u2551\n      \u2551 Marca da TV    : ").concat(this.brand, "           \u2551\n      \u2551 Estado da TV   : ").concat(this.power ? "Ligada" : "Desligada", "            \u2551\n      \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n    "));
+    };
+    return RemoteControl;
+}());
+//#region Controle1
+var remote1 = new RemoteControl("Preto", "Samsung", 20, 1);
 remote1.togglePower();
 remote1.showControlStatus();
 remote1.increaseVolume();
@@ -64,62 +120,9 @@ remote1.increaseVolume();
 remote1.changeChannel(5);
 remote1.decreaseVolume();
 remote1.showControlStatus();
-var remote2 = {
-    color: "Branco",
-    brand: "LG",
-    volume: 10,
-    channel: 3,
-    power: false,
-    showControlStatus: function () {
-        console.log("\n      \u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n      \u2551         STATUS DO CONTROLE         \u2551\n      \u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n      \u2551 Volume         : ".concat(this.volume, "                \u2551\n      \u2551 Canal          : ").concat(this.channel, "                 \u2551\n      \u2551 Cor do Controle: ").concat(this.color, "            \u2551\n      \u2551 Marca da TV    : ").concat(this.brand, "                \u2551\n      \u2551 Estado da TV   : ").concat(this.power ? "Ligada" : "Desligada", "            \u2551\n      \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n    "));
-    },
-    togglePower: function () {
-        this.power = !this.power;
-        console.log("A TV est\u00E1 ".concat(this.power ? "Ligada" : "Desligada"));
-    },
-    increaseVolume: function () {
-        if (this.power) {
-            if (this.volume < 100) {
-                this.volume += 10;
-                console.log("Volume aumentado para: ".concat(this.volume));
-            }
-            else {
-                console.log("O volume já está no máximo!");
-            }
-        }
-        else {
-            console.log("A TV ".concat(this.brand, " est\u00E1 Desligada! Ligue para conseguir interagir."));
-        }
-    },
-    decreaseVolume: function () {
-        if (this.power) {
-            if (this.volume > 0) {
-                this.volume -= 10;
-                console.log("Volume reduzido para: ".concat(this.volume));
-            }
-            else {
-                console.log("O volume já está no mínimo!");
-            }
-        }
-        else {
-            console.log("A TV ".concat(this.brand, " est\u00E1 Desligada! Ligue para conseguir interagir."));
-        }
-    },
-    changeChannel: function (newChannel) {
-        if (this.power) {
-            if (newChannel > 0) {
-                this.channel = newChannel;
-                console.log("Canal alterado para: ".concat(this.channel));
-            }
-            else {
-                console.log("Número de canal inválido!");
-            }
-        }
-        else {
-            console.log("A TV ".concat(this.brand, " est\u00E1 Desligada! Ligue para conseguir interagir."));
-        }
-    },
-};
+//#endregion
+//#region Controle2
+var remote2 = new RemoteControl("Cinza", "Philips", 10, 3);
 remote2.togglePower();
 remote2.showControlStatus();
 remote2.increaseVolume();
@@ -132,53 +135,44 @@ remote2.changeChannel(7);
 remote2.decreaseVolume();
 remote2.decreaseVolume();
 remote2.showControlStatus();
-var airControl = {
-    color: "Cinza",
-    brand: "Elgin",
-    volume: 0,
-    channel: 0,
-    power: false,
-    temperature: 19,
-    mode: "cool",
-    showControlStatus: function () {
-        console.log("\n      \u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n      \u2551  STATUS DO CONTROLE DE AR-COND.    \u2551\n      \u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n      \u2551 Temperatura    : ".concat(this.temperature, "\u00B0C              \u2551\n      \u2551 Modo           : ").concat(this.mode.toUpperCase(), "              \u2551\n      \u2551 Cor do Controle: ").concat(this.color, "             \u2551\n      \u2551 Marca          : ").concat(this.brand, "             \u2551\n      \u2551 Estado         : ").concat(this.power ? "Ligado" : "Desligado", "            \u2551\n      \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n    "));
-    },
-    togglePower: function () {
-        this.power = !this.power;
-        console.log("O Ar-condicionado est\u00E1 ".concat(this.power ? "Ligado" : "Desligado"));
-    },
-    increaseVolume: function () {
-        console.log("Esta função não é aplicável ao ar-condicionado.");
-    },
-    decreaseVolume: function () {
-        console.log("Esta função não é aplicável ao ar-condicionado.");
-    },
-    changeChannel: function () {
-        console.log("Esta função não é aplicável ao ar-condicionado.");
-    },
-    changeTemperature: function (newTemperature) {
-        if (this.power) {
+//#endregion
+//#region Controle Ar
+var AirConditionerRemoteControl = /** @class */ (function (_super) {
+    __extends(AirConditionerRemoteControl, _super);
+    function AirConditionerRemoteControl() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.temperature = 16;
+        return _this;
+    }
+    AirConditionerRemoteControl.prototype.showControlStatus = function () {
+        console.log("\n      \u2554\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2557\n      \u2551  STATUS DO CONTROLE DO AR-COND.    \u2551\n      \u2560\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2563\n      \u2551 Temperatura    : ".concat(this.temperature, "\u00B0C              \u2551\n      \u2551 Modo           : ").concat(this.mode, "              \u2551\n      \u2551 Cor do Controle: ").concat(this.getColor, "            \u2551\n      \u2551 Marca          : ").concat(this.getBrand, "             \u2551\n      \u2551 Estado         : ").concat(this.getPower, "            \u2551\n      \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u2550\u255D\n    "));
+    };
+    AirConditionerRemoteControl.prototype.changeTemperature = function (newTemperature) {
+        if (this.getPower) {
             this.temperature += newTemperature;
             console.log("Temperatura ajustada para: ".concat(this.temperature, "\u00B0C"));
         }
         else {
             console.log("Ligue o Ar-condicionado para ajustar a temperatura.");
         }
-    },
-    changeMode: function (newMode) {
-        if (this.power) {
+    };
+    AirConditionerRemoteControl.prototype.changeMode = function (newMode) {
+        if (this.getPower) {
             this.mode = newMode;
-            console.log("Modo alterado para: ".concat(this.mode.toUpperCase()));
+            console.log("Modo alterado para: ".concat(this.mode));
         }
         else {
             console.log("Ligue o Ar-condicionado para alterar o modo.");
         }
-    },
-};
+    };
+    return AirConditionerRemoteControl;
+}(RemoteControl));
+var airControl = new AirConditionerRemoteControl("Branco", "Elgin", 0, 0);
 airControl.togglePower();
+airControl.changeMode(Mode.Heat);
+airControl.changeTemperature(10);
 airControl.showControlStatus();
-airControl.changeTemperature(2);
-airControl.changeMode("heat");
+airControl.changeMode(Mode.Cool);
 airControl.changeTemperature(-5);
 airControl.showControlStatus();
 //#endregion
--- index.ts
+++ index.ts
@@ -1,43 +1,58 @@
-interface RemoteControl {
-  color: string;
-  brand: string;
-  volume: number;
-  channel: number;
-  power: boolean;
+enum Mode {
+  Cool = "COOL",
+  Heat = "HEAT",
+  Fan = "FAN"
+}
 
-  showControlStatus(): void;
+enum Power {
+  On = "Ligado",
+  Off = "Desligado",
+}
+
+interface Control {
+  // variáveis aqui terão que ser públicas na classe por conta da interface/type não suportar modificadores de acesso
   togglePower(): void;
   increaseVolume(): void;
   decreaseVolume(): void;
-  changeChannel(channel: number): void;
+  changeChannel(newChannel: number): void;
+  showControlStatus(): void;
 }
 
-//#region Controle1
-const remote1: RemoteControl = {
-  color: "Preto",
-  brand: "Samsung",
-  volume: 20,
-  channel: 1,
-  power: false,
+class RemoteControl implements Control {
+  private color: string;
+  private brand: string;
+  private volume: number;
+  private channel: number;
+  private power: Power;
 
-  showControlStatus() {
-    console.log(`
-      ╔════════════════════════════════════╗
-      ║         STATUS DO CONTROLE         ║
-      ╠════════════════════════════════════╣
-      ║ Volume         : ${this.volume}                ║
-      ║ Canal          : ${this.channel}                 ║
-      ║ Cor do Controle: ${this.color}             ║
-      ║ Marca da TV    : ${this.brand}           ║
-      ║ Estado da TV   : ${this.power ? "Ligada" : "Desligada"}            ║
-      ╚════════════════════════════════════╝
-    `);
-  },
+  get getPower(): Power {
+    return this.power;
+  }
+
+  get getColor(): string {
+    return this.color;
+  }
+
+  get getBrand(): string {
+    return this.brand;
+  }
+
+  constructor(cor: string, marca: string, volume: number, canal: number) {
+    this.color = cor;
+    this.brand = marca;
+    this.volume = volume;
+    this.channel = canal;
+    this.power = Power.Off;
+  }
 
   togglePower() {
-    this.power = !this.power;
-    console.log(`A TV está ${this.power ? "Ligada" : "Desligada"}`);
-  },
+    if (this.power === Power.Off) {
+      this.power = Power.On;
+    } else {
+      this.power = Power.Off;
+    }
+    console.log(`O Dispositvo está ${this.power}`);
+  }
 
   increaseVolume() {
     if (this.power) {
@@ -50,7 +65,7 @@ const remote1: RemoteControl = {
     } else {
       console.log(`A TV ${this.brand} está Desligada! Ligue para conseguir interagir.`);
     }
-  },
+  }
 
   decreaseVolume() {
     if (this.power) {
@@ -63,7 +78,7 @@ const remote1: RemoteControl = {
     } else {
       console.log(`A TV ${this.brand} está Desligada! Ligue para conseguir interagir.`);
     }
-  },
+  }
 
   changeChannel(newChannel: number) {
     if (this.power) {
@@ -76,8 +91,25 @@ const remote1: RemoteControl = {
     } else {
       console.log(`A TV ${this.brand} está Desligada! Ligue para conseguir interagir.`);
     }
-  },
-};
+  }
+
+  showControlStatus() {
+    console.log(`
+      ╔════════════════════════════════════╗
+      ║       STATUS DO CONTROLE TV        ║
+      ╠════════════════════════════════════╣
+      ║ Volume         : ${this.volume}                ║
+      ║ Canal          : ${this.channel}                 ║
+      ║ Cor do Controle: ${this.color}             ║
+      ║ Marca da TV    : ${this.brand}           ║
+      ║ Estado da TV   : ${this.power ? "Ligada" : "Desligada"}            ║
+      ╚════════════════════════════════════╝
+    `);
+  }
+}
+
+//#region Controle1
+const remote1 = new RemoteControl("Preto", "Samsung", 20, 1);
 
 remote1.togglePower();
 remote1.showControlStatus();
@@ -91,85 +123,7 @@ remote1.showControlStatus();
 //#endregion
 
 //#region Controle2
-type RemoteControlType = {
-  color: string;
-  brand: string;
-  volume: number;
-  channel: number;
-  power: boolean;
-
-  showControlStatus(): void;
-  togglePower(): void;
-  increaseVolume(): void;
-  decreaseVolume(): void;
-  changeChannel(newChannel: number): void;
-};
-
-const remote2: RemoteControlType = {
-  color: "Branco",
-  brand: "LG",
-  volume: 10,
-  channel: 3,
-  power: false,
-
-  showControlStatus() {
-    console.log(`
-      ╔════════════════════════════════════╗
-      ║         STATUS DO CONTROLE         ║
-      ╠════════════════════════════════════╣
-      ║ Volume         : ${this.volume}                ║
-      ║ Canal          : ${this.channel}                 ║
-      ║ Cor do Controle: ${this.color}            ║
-      ║ Marca da TV    : ${this.brand}                ║
-      ║ Estado da TV   : ${this.power ? "Ligada" : "Desligada"}            ║
-      ╚════════════════════════════════════╝
-    `);
-  },
-
-  togglePower() {
-    this.power = !this.power;
-    console.log(`A TV está ${this.power ? "Ligada" : "Desligada"}`);
-  },
-
-  increaseVolume() {
-    if (this.power) {
-      if (this.volume < 100) {
-        this.volume += 10;
-        console.log(`Volume aumentado para: ${this.volume}`);
-      } else {
-        console.log("O volume já está no máximo!");
-      }
-    } else {
-      console.log(`A TV ${this.brand} está Desligada! Ligue para conseguir interagir.`);
-    }
-  },
-
-  decreaseVolume() {
-    if (this.power) {
-      if (this.volume > 0) {
-        this.volume -= 10;
-        console.log(`Volume reduzido para: ${this.volume}`);
-      } else {
-        console.log("O volume já está no mínimo!");
-      }
-    } else {
-      console.log(`A TV ${this.brand} está Desligada! Ligue para conseguir interagir.`);
-    }
-  },
-
-  changeChannel(newChannel: number) {
-    if (this.power) {
-      if (newChannel > 0) {
-        this.channel = newChannel;
-        console.log(`Canal alterado para: ${this.channel}`);
-      } else {
-        console.log("Número de canal inválido!");
-      }
-    } else {
-      console.log(`A TV ${this.brand} está Desligada! Ligue para conseguir interagir.`);
-    }
-  },
-};
+const remote2 = new RemoteControl("Cinza", "Philips", 10, 3);
 
 remote2.togglePower();
 remote2.showControlStatus();
@@ -186,76 +140,50 @@ remote2.showControlStatus();
 //#endregion
 
 //#region Controle Ar
-type AirConditionerRemoteControl = RemoteControl & {
-  temperature: number;
-  mode: "cool" | "heat" | "fan";
-  changeTemperature(delta: number): void;
-  changeMode(newMode: "cool" | "heat" | "fan"): void;
-};
+class AirConditionerRemoteControl extends RemoteControl {
+  private temperature: number = 16;
+  private mode: Mode;
 
-const airControl: AirConditionerRemoteControl = {
-  color: "Cinza",
-  brand: "Elgin",
-  volume: 0, 
-  channel: 0, 
-  power: false,
-  temperature: 19,
-  mode: "cool",
-
-  showControlStatus() {
+   showControlStatus() {
     console.log(`
       ╔════════════════════════════════════╗
-      ║  STATUS DO CONTROLE DE AR-COND.    ║
+      ║  STATUS DO CONTROLE DO AR-COND.    ║
       ╠════════════════════════════════════╣
       ║ Temperatura    : ${this.temperature}°C              ║
-      ║ Modo           : ${this.mode.toUpperCase()}              ║
-      ║ Cor do Controle: ${this.color}             ║
-      ║ Marca          : ${this.brand}             ║
-      ║ Estado         : ${this.power ? "Ligado" : "Desligado"}            ║
+      ║ Modo           : ${this.mode}              ║
+      ║ Cor do Controle: ${this.getColor}            ║
+      ║ Marca          : ${this.getBrand}             ║
+      ║ Estado         : ${this.getPower}            ║
       ╚════════════════════════════════════╝
     `);
-  },
-
-  togglePower() {
-    this.power = !this.power;
-    console.log(`O Ar-condicionado está ${this.power ? "Ligado" : "Desligado"}`);
-  },
-
-  increaseVolume() {
-    console.log("Esta função não é aplicável ao ar-condicionado.");
-  },
-
-  decreaseVolume() {
-    console.log("Esta função não é aplicável ao ar-condicionado.");
-  },
-
-  changeChannel() {
-    console.log("Esta função não é aplicável ao ar-condicionado.");
-  },
+  }
 
   changeTemperature(newTemperature: number) {
-    if (this.power) {
+    if (this.getPower) {
       this.temperature += newTemperature;
       console.log(`Temperatura ajustada para: ${this.temperature}°C`);
     } else {
       console.log("Ligue o Ar-condicionado para ajustar a temperatura.");
     }
-  },
+  }
 
-  changeMode(newMode: "cool" | "heat" | "fan") {
-    if (this.power) {
+  changeMode(newMode: Mode) {
+    if (this.getPower) {
       this.mode = newMode;
-      console.log(`Modo alterado para: ${this.mode.toUpperCase()}`);
+      console.log(`Modo alterado para: ${this.mode}`);
     } else {
       console.log("Ligue o Ar-condicionado para alterar o modo.");
     }
-  },
-};
+  }
+}
+
+const airControl = new AirConditionerRemoteControl("Branco", "Elgin", 0, 0);
 
 airControl.togglePower();
+airControl.changeMode(Mode.Heat);
+airControl.changeTemperature(10);
 airControl.showControlStatus();
-airControl.changeTemperature(2);
-airControl.changeMode("heat");
+airControl.changeMode(Mode.Cool);
 airControl.changeTemperature(-5);
 airControl.showControlStatus();
 //#endregion
